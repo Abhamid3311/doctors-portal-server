5d877c55eb52aeb176c66e3239c56095e661f652
Hamid <www.abdulhamid3311@gmail.com>
2022-06-25 12:40:43 +0600

    added payment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,7 +32,7 @@ function verifyJWT(req, res, next) {
         req.decoded = decoded;
         next();
     });
-}
+};
 
 
 
@@ -40,6 +43,7 @@ async function run() {
         const bookingCollection = client.db("doctors_portal").collection("bookings");
         const userCollection = client.db("doctors_portal").collection("users");
         const doctorsCollection = client.db("doctors_portal").collection("doctors");
+        const paymentsCollection = client.db("doctors_portal").collection("payments");
 
 
         const verifyAdmin = async (req, res, next) => {
@@ -50,7 +54,23 @@ async function run() {
             } else {
                 return res.status(403).send({ message: "Forbidden Access" });
             }
-        }
+        };
+
+        //Post Payment Intent
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+
+
+        });
 
         //GET SERVICES
         app.get('/service', async (req, res) => {
@@ -135,8 +155,16 @@ async function run() {
             } else {
                 return res.status(403).send({ message: "Forbidden Access" });
             }
-
         });
+
+        //Get single booking for payment
+        app.get('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
+        });
+
         //POST BOOKING
         app.post('/booking', async (req, res) => {
             const booking = req.body;
@@ -148,17 +176,37 @@ async function run() {
             const result = await bookingCollection.insertOne(booking);
             res.send({ success: true, result });
         });
+
+        //Patch Booking
+        app.patch('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            };
+            const result = await paymentsCollection.insertOne(payment);
+            const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+
+            res.send(updatedDoc);
+        });
+
         //POST Doctor
         app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
             const doctor = req.body;
             const result = await doctorsCollection.insertOne(doctor);
             res.send(result);
         });
+
         //GET DOCTORS
         app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
             const doctors = await doctorsCollection.find().toArray();
             res.send(doctors);
         });
+
         //Delete DOCTORS
         app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
